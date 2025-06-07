1bcd9fc0d97bce71ae290240e4fd17309286fb77
Marco Reyes <114314498+mreydev@users.noreply.github.com>
2025-06-07 02:02:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ var w = c.width = window.innerWidth,
 		
 		opts = {
 			strings: ['FELIZ', 'CUMPLEAÑOS', '❤️MONI❤️'],
-			charSize: 20,
-			charSpacing: 25,
-			lineHeight: 30,
+			charSize: 30,
+			charSpacing: 35,
+			lineHeight: 40,
 			
 			cx: w / 2,
 			cy: h / 2,
